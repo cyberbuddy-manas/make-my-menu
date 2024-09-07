58f7d336946632624cc558ff0ca2ee41f9512595
Japsimran Singh <japsimrans13@gmail.com>
2024-09-07 11:45:20 +0530

    expo setup

--- apps/backend/src/models/userModel.ts
+++ apps/backend/src/models/userModel.ts
@@ -1,23 +1,25 @@
 // User Model
 import { Schema, model } from 'mongoose';
-import bcryptjs from 'bcryptjs';
 
 interface IUser {
     email: string;
     phoneNumber: number;
     isOnHold: boolean;
+    loginToken: {
+        otp: string;
+        expires: Date;
+    };
   }
 
 const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
     phoneNumber: { type: Number, required: true, unique: true },
-    password: { type: String, required: true },
     // isOnHold is used to stop the user from logging in
     isOnHold: { type: Boolean, default: false },
     // Saving the token in DB for SSO
     token: { type: String },
     // reset password token for reset password
-    resetPasswordToken: {
+    loginToken: {
         otp: { type: String },
         expires: { type: Date }
     },
@@ -26,11 +28,6 @@ const userSchema = new Schema({
     { timestamps: true }
 );
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcryptjs.hash(this.password, 12);
-    next();
-});
 
 const User = model<IUser>('User', userSchema);
 
