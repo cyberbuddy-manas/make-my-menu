f2119d1917c5f693947e3012067e514c3b473231
Japsimran Singh <japsimrans13@gmail.com>
2024-09-08 07:20:43 +0530

    ssr code update

--- apps/backend/src/controllers/menuController.ts
+++ apps/backend/src/controllers/menuController.ts
@@ -1,20 +1,34 @@
-import Restaurant from "../models/restaurantModel";
+import Restaurant from '../models/restaurantModel';
 import { Request, Response } from 'express';
 
 export const getMenu = async (req: Request, res: Response) => {
     try {
         // TODO: check request subdomain
+        // console.log(req)
+        const host = req.host;
+        let restaurant;
+        if (host == 'localhost') {
+            restaurant = await Restaurant.findOne({ subDomain: 'jp' });
+        }
+
         const origin = req.headers.origin;
-        console.log(origin, "origin");
+        console.log(origin, "origin")
+        // TODO: make it dynamic for all subdomains
         // const domainName = origin.split('//')[1].split(':')[0];
-        
         // const subDomain = domainName.split('.')[0];
         // let restaurant;
-    // if (subDomain == 'localhost'){
-       const  restaurant = await Restaurant.findOne({ subDomain: 're' });
-    // } else {
+            // } else {
     //     restaurant = await Restaurant.findOne({ subDomain });
     // }
+
+       console.log(restaurant, "restaurant");
+       if (!restaurant) {
+        return res.status(404).send({
+            message: 'Restaurant not found',
+            success: false,
+        });
+    }
+
     // TODO: format the menu
     // Get unique categories
     const categories = new Set();
@@ -22,16 +36,13 @@ export const getMenu = async (req: Request, res: Response) => {
     (restaurant.menu as any[]).forEach((item: any) => {
         categories.add(item.category);
     });
-    console.log(restaurant);
-    if (!restaurant) {
-        return res.status(404).send({
-            message: 'Restaurant not found',
-            success: false,
-        });
-    }
+    // convert set to array
+    const categoriesArray = Array.from(categories);
+    console.log(categories, "categories")
+
       
 
-    return res.render('menu1/index', {menu: restaurant.menu, layout: false});
+    return res.render('menu1/index', {categories: categoriesArray, menu: restaurant.menu, layout: false});
     } catch (error) {
         console.log(error);
         return res.status(500).send({
